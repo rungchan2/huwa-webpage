32716aeedc2de625b9c20d83c4fa9260eb24223b
Heechan <heechan@Heechanui-MacBookPro.local>
2024-07-19 19:28:50 +0900

    og image :type added

--- env.ts
+++ env.ts
@@ -4,4 +4,5 @@ export const EnvVars = {
   OG_IMAGES_URL: '/public/ogimage.png',
   URL: 'https://huwa-webpage.vercel.app/',
   DESC: '브랜딩 웹 에이전시 후와입니다.',
+  MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
